1d00aec7f06f219d1918e2bdd16526b8932407d2
Dora_The_Explorer_Is_Dead <umama.m.edu1@gmail.com>
2025-05-20 20:38:32 +0500

    Delete Activation_Softmax.cpp

--- Activation_Softmax.cpp
+++
@@ -1,5 +0,0 @@
-#include "..\headers\Activation_Softmax.hpp"
-
-void Activation_Softmax::forward(vector<vector<double>> inputs) {
-    output = Mat_Div(convert_to_exp(inputs), broadcast(sum(convert_to_exp(inputs), 1), inputs[0].size()));
-}
